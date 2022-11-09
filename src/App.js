8591edf596fbdc934817a1b40dab878fafa4862c
ThauThien <solarlight1912@gmail.com>
2022-11-09 08:29:02 +0700

    Updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { DefaultLayout } from '~/components/Layout';
 
 function App() {
   return (
-    <Router>
+    <Router basename='/tiktok-ui'>
       <div className="App">
         <Routes>
           {publicRoutes.map((route, index) => {
